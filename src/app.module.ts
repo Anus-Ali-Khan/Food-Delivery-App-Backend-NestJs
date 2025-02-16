0632b5e0005518e14f4e2808894445b9c35727e7
Anus Ali Khan <anusalikhan1405@gmail.com>
2025-02-16 23:58:25 +0500

    [WIP]: setting prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegisterController } from './register/register.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, RegisterController],
   providers: [AppService],
 })
 export class AppModule {}
