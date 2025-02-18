a530464f140e2b4e9d3397795996ca7fb72cae16
Anus Ali Khan <anusalikhan1405@gmail.com>
2025-02-18 11:28:42 +0500

    [WIP]: authorization

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegisterController } from './register/register.controller';
 import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, RegisterController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  controllers: [AppController, RegisterController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
